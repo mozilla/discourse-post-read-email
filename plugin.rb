4aa041545ed300182a178b94edcc7d29b9c8cffb
Leo McArdle <leomcardle@gmail.com>
2018-06-08 21:34:49 +0100

    only working on successful sends, need to write tests

--- plugin.rb
+++ plugin.rb
@@ -6,17 +6,27 @@
 
 DiscoursePluginRegistry.serialized_current_user_fields << 'mark_post_as_read_on_email'
 
-module JobsUserEmailExtensions
-  def message_for_email(user, post, type, notification, args = nil)
-    message, skip_reason = super
-    if message && user.custom_fields['mark_post_as_read_on_email'] == 'true'
-      notification.update(read: true)
-      TopicUser.update_last_read(user, post.topic&.id, post.post_number, 0, 0)
+module EmailSenderExtensions
+
+  def send
+    email_log = super
+    if email_log.skipped != true
+      if @user&.custom_fields['mark_post_as_read_on_email'] == 'true'
+        post = Post.find(email_log.post_id)
+        Notification.where(
+          user_id: @user.id,
+          topic_id: post.topic_id,
+          post_number: post.post_number,
+          notification_type: Notification.types[@email_type.to_s.sub('user_', '').to_sym]
+        ).update(read: true)
+        TopicUser.update_last_read(@user, post.topic_id, post.post_number, 0, 0)
+      end
     end
-    [message, skip_reason]
+    email_log
   end
+
 end
 
 after_initialize do
-  Jobs::UserEmail.prepend JobsUserEmailExtensions
+  Email::Sender.prepend EmailSenderExtensions
 end
