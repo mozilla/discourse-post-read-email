c86d4899df48fb19e49ec6eb304ff6b5e6a947c1
Leo McArdle <leomcardle@gmail.com>
2018-06-08 20:51:15 +0100

    working, but even on failed email sends

--- plugin.rb
+++ plugin.rb
@@ -5,3 +5,18 @@
 # url: https://github.com/mozilla/discourse-post-read-email
 
 DiscoursePluginRegistry.serialized_current_user_fields << 'mark_post_as_read_on_email'
+
+module JobsUserEmailExtensions
+  def message_for_email(user, post, type, notification, args = nil)
+    message, skip_reason = super
+    if message && user.custom_fields['mark_post_as_read_on_email'] == 'true'
+      notification.update(read: true)
+      TopicUser.update_last_read(user, post.topic&.id, post.post_number, 0, 0)
+    end
+    [message, skip_reason]
+  end
+end
+
+after_initialize do
+  Jobs::UserEmail.prepend JobsUserEmailExtensions
+end
