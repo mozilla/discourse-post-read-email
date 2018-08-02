6961a75a930a317584952ed23e9275090f3893cf
Leo McArdle <leomcardle@gmail.com>
2018-08-02 18:12:01 +0100

    hotfix for failing email jobs

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: post-read-email
 # about: A plugin to give users the option of marking posts as read when emailed
-# version: 0.0.1
+# version: 0.0.2
 # authors: Leo McArdle
 # url: https://github.com/mozilla/discourse-post-read-email
 
@@ -10,19 +10,17 @@ module EmailSenderExtensions
 
   def send
     email_log = super
-    if email_log&.skipped != true
-      if @user&.custom_fields['mark_post_as_read_on_email'] == 't' ||
-         @user&.custom_fields['mark_post_as_read_on_email'] == 'true'
-        post = Post.find(email_log.post_id)
-        if post
-          Notification.where(
-            user_id: @user.id,
-            topic_id: post.topic_id,
-            post_number: post.post_number,
-            notification_type: Notification.types[@email_type.to_s.sub('user_', '').to_sym]
-          ).update(read: true)
-          TopicUser.update_last_read(@user, post.topic_id, post.post_number, 0, 0)
-        end
+    if @user&.custom_fields['mark_post_as_read_on_email'] == 't' ||
+       @user&.custom_fields['mark_post_as_read_on_email'] == 'true'
+      post = Post.find(email_log.post_id)
+      if post
+        Notification.where(
+          user_id: @user.id,
+          topic_id: post.topic_id,
+          post_number: post.post_number,
+          notification_type: Notification.types[@email_type.to_s.sub('user_', '').to_sym]
+        ).update(read: true)
+        TopicUser.update_last_read(@user, post.topic_id, post.post_number, 0, 0)
       end
     end
     email_log
